c763e93b9c0ebc02f3ddb6083f6ae676387e7e60
lau-byte <lau.teller.21@lehre.mosbach.dhbw.de>
2022-11-25 19:38:31 +0100

    Formular eingefügt
    Formular auf der "Geschenke erstellen" Seite eingefügt

--- teller-laura-geschenkeliste/resources/views/add_geschenke.blade.php
+++ teller-laura-geschenkeliste/resources/views/add_geschenke.blade.php
@@ -13,7 +13,51 @@
 
                 <div class="card-body">
 
-                    
+                <form method="POST" action="{{ route('geschenke.store') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="title" class="col-form-label text-md-right">Geschenk</label>
+
+                            <input id="title" type="title" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('email') }}" autocomplete="title" autofocus>
+
+                            @error('title')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="description" class="col-form-label text-md-right">Beschreibung</label>
+
+                            <textarea name="description" id="description" cols="30" rows="10" class="form-control @error('password') is-invalid @enderror" autocomplete="description" value="{{ old('description') }}"></textarea>
+
+                            @error('description')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="completed" id="completed" value="{{ old('completed')}}">
+
+                                <label class="form-check-label" for="completed">
+                                    Besorgt?
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Speichern
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
